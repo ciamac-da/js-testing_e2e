2d5473c8d6d959dd17222f309b4abca957a788c7
ciamac-da <ciamac.davoudi@gmail.com>
2021-10-03 10:51:20 +0200

    Verified by Marti4n v1.0.2

---
+++ README.md
@@ -0,0 +1,1 @@
+https://ciamac-da.github.io/js-testing_e2e/
--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer")
 const { generateText, checkAndGenerate } = require("./util");
 
 
@@ -12,3 +13,20 @@ test('Should generate a valid text output', () => {
  expect(text).toBe("Cia (34 years old)")
 });
 
+test("Should create an element with text and correct class",async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args: ["--window-size=1920,1080"]
+    })
+    const page = await browser.newPage();
+    await page.goto("file:///home/cia/Documents/Programming/MyProgramming/JS/js-testing_e2e/index.html")
+    
+    await page.click("input#name")
+    await page.type("input#name", "Cia")
+    await page.click("input#age")
+    await page.type("input#age", "34")
+    await page.click("#btnAddUser")
+    const finalText = await page.$eval(".user-item", el => el.textContent)
+    expect(finalText).toBe("Cia (34 years old)")
+}, 10000)
